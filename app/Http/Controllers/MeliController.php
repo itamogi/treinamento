ad1c56173e3ed98dae8428ebcbb51f825b5dda55
unknown <psymics@gmail.com>
2020-02-15 12:11:25 -0300

    'subindo'

--- app/Http/Controllers/MeliController.php
+++ app/Http/Controllers/MeliController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Aws\Sqs\SqsClient; 
+use Aws\Exception\AwsException;
+use Illuminate\Http\Request;
+
 class MeliController extends Controller
 {
     /**
@@ -14,7 +18,66 @@ class MeliController extends Controller
         //
     }
 	
-	public function enviaSQS(){
+	public function enviaSQS( Request $request ){
+		
+		//dd( $request->all() );
+		
+		$client = SqsClient::factory([
+			'credentials' => [ 'key' => 'AKIAQPFHDWZWVYYDFF6Y'
+									, 'secret' => '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' ],
+			'region' => 'us-east-1',
+			'version' => '2012-11-05'
+		]);
+		
+		$params = [
+			
+			'MessageAttributes' => [
+				"titulo" => [
+					'DataType' => "String",
+					'StringValue' => $request->titulo
+				],
+				"categoria" => [
+					'DataType' => "String",
+					'StringValue' => $request->categoria
+				],
+				"preco" => [
+					'DataType' => "String",
+					'StringValue' => $request->preco
+				],
+				"quantidade" => [
+					'DataType' => "String",
+					'StringValue' => $request->quantidade
+				],
+				"descricao" => [
+					'DataType' => "String",
+					'StringValue' => $request->descricao
+				],
+				"token" => [
+					'DataType' => "String",
+					'StringValue' => $request->token
+				],
+				"fotos" => [
+					'DataType' => "String",
+					'StringValue' => json_encode( $request->fotos )
+				]
+			],
+			'MessageBody' => "Invocacao do MAU.",
+			'QueueUrl' => 'https://sqs.us-east-1.amazonaws.com/032562722413/sqstreinamento'
+		];
+		
+		try {
+			for( $i = 0; $i < 10; $i++ ){
+				$result = $client->sendMessage($params);
+				dump('DISPARADO');
+			}
+			
+		} catch (AwsException $e) {
+			// output error message if fails
+			dd($e->getMessage());
+		}catch(Exception $e){
+			dd($e->getMessage());
+			}
+		 
 		
 		dd('funcionando blau blau');
 	
--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-$router->get('/test' , 'MeliController@enviaSQS');
+$router->post('/enfilera' , 'MeliController@enviaSQS');
