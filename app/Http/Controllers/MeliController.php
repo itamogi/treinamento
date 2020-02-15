20501bcf5f3e52bc06ae193613cfe47b176aaf99
unknown <psymics@gmail.com>
2020-02-15 09:53:38 -0300

    asd

---
+++ app/Http/Controllers/MeliController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class MeliController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+	
+	public function enviaSQS(){
+		
+		dd('funcionando');
+	
+		
+	}
+
+    //
+}
